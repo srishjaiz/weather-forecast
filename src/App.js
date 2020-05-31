29ce35dee25298d498717aa5a79957cb52644b01
srishjaiz <srishjaiz@gmail.com>
2020-05-31 21:35:14 +0530

    design header component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import classes from './App.module.css';
+import Logo from './components/Logo/Logo';
 // import axios from 'axios';
 
 function App() {
   return (
-    <>
-    <header>
-      Logo
-    </header>
-    <main>
-      Main
-    </main>
-    </>
+    <div className={classes.App}>
+      <header>
+        <Logo/> 
+      </header>
+      <main>
+        Main
+      </main>
+    </div>
   );
 }
 
